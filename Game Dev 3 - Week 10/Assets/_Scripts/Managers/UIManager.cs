d84bf491328cf87b034e06a3b8b904e8471f76e0
jerryjerrycheng0 <114728896+jerryjerrycheng0@users.noreply.github.com>
2024-11-27 15:56:13 +0000

    Challenge 1 Done

--- Game Dev 3 - Week 10/Assets/_Scripts/Managers/UIManager.cs
+++ Game Dev 3 - Week 10/Assets/_Scripts/Managers/UIManager.cs
@@ -5,22 +5,24 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
-public class UIManager : MonoBehaviour
+namespace GameDevWithMarco.Managers
 {
-    //Objects
-    public  Text countdownText; 
-    public  TextMeshProUGUI scoreTextBox;
-    public  TextMeshProUGUI livesTextBox;
-    public  Text            timeTextBox;
-    public  GameObject minusOneLifePosition;
-    public  GameObject minusOneLife;
-    public  GameObject plusOneLife;
-    //Variables
-    public string testString = "Test String";
-    private Text gameOverScore;
-    private Text gameOverScoreComment;
+    public class UIManager : MonoBehaviour
+    {
+        //Objects
+        public Text countdownText;
+        public TextMeshProUGUI scoreTextBox;
+        public TextMeshProUGUI livesTextBox;
+        public Text timeTextBox;
+        public GameObject minusOneLifePosition;
+        public GameObject minusOneLife;
+        public GameObject plusOneLife;
+        //Variables
+        public string testString = "Test String";
+        private Text gameOverScore;
+        private Text gameOverScoreComment;
 
-    private Dictionary<int, string> scoreComments = new Dictionary<int, string>
+        private Dictionary<int, string> scoreComments = new Dictionary<int, string>
     {
         { 0, "HUMAN, TRY AGAIN" },
         { 20000, "LOW SKILL DETECTED" },
@@ -37,115 +39,116 @@ public class UIManager : MonoBehaviour
     };
 
 
-    private void Awake()
-    {
-        Initialisation();
-    }
-    
-    
-    
-    void Update()
-    {        
-        ScoreAndTimerUpdater();
-        ChangeTimerColor();               
-    }
-
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-
-    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)    
-    {
-        if (SceneManager.GetActiveScene().name == "scn_GameOver")
+        private void Awake()
         {
-            GameOverInitialisation();
+            Initialisation();
         }
-    }
 
-    public void ScoreAndTimerUpdater()
-    {
 
-        if (scoreTextBox != null)
+
+        void Update()
         {
+            ScoreAndTimerUpdater();
+            ChangeTimerColor();
+        }
+
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (SceneManager.GetActiveScene().name == "scn_GameOver")
+            {
+                GameOverInitialisation();
+            }
+        }
+
+        public void ScoreAndTimerUpdater()
+        {
+
+            if (scoreTextBox != null)
+            {
+                scoreTextBox.text = "SCORE:" + GameManager.Instance.score.ToString();
+            }
+            if (livesTextBox != null)
+            {
+                livesTextBox.text = "LIVES: " + GameManager.Instance.lives.ToString("f0");
+            }
+            if (timeTextBox != null)
+            {
+                timeTextBox.text = "TIME: " + GameManager.Instance.playTime.ToString("f0");
+            }
+
+        }
+
+        private void ChangeTimerColor()
+        {
+            if (livesTextBox != null)
+            {
+                if (GameManager.Instance.lives < 2)
+                {
+                    livesTextBox.color = new Color(1, 0, 0);
+                }
+            }
+
+        }
+
+
+
+
+        private void FinalComment()
+        {
+            if (gameOverScoreComment == null)
+                return; // Code Blocker
+
+            foreach (var scoreThreshold in scoreComments.Keys)
+            {
+                if (GameManager.Instance.score < scoreThreshold)
+                {
+                    gameOverScoreComment.text = scoreComments[scoreThreshold] + " - RESTART SESSION";
+                    return;
+                }
+            }
+        }
+
+        public void MinusOneLifeFeedback()
+        {
+            var minusFeedback = Instantiate(minusOneLife, minusOneLifePosition.transform.position, Quaternion.identity) as GameObject;
+            minusFeedback.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            minusFeedback.transform.position = minusOneLifePosition.transform.position;
+            Destroy(minusFeedback, 2f);
+
+        }
+
+        public void PlusOneLifeFeedback()
+        {
+            var plusFeedback = Instantiate(plusOneLife, minusOneLifePosition.transform.position, Quaternion.identity) as GameObject;
+            plusFeedback.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            plusFeedback.transform.position = minusOneLifePosition.transform.position;
+            Destroy(plusFeedback, 2f);
+        }
+
+
+        private void Initialisation()
+        {
+            scoreTextBox = GameObject.Find("txtPro_Score").GetComponent<TextMeshProUGUI>();
             scoreTextBox.text = "SCORE:" + GameManager.Instance.score.ToString();
-        }
-        if (livesTextBox != null)
-        {
+
+            livesTextBox = GameObject.Find("txtPro_Lives").GetComponent<TextMeshProUGUI>();
             livesTextBox.text = "LIVES: " + GameManager.Instance.lives.ToString("f0");
+
+            timeTextBox = GameObject.Find("txt_Timer").GetComponent<Text>();
+
         }
-        if (timeTextBox != null)
+
+        private void GameOverInitialisation()
         {
-            timeTextBox.text = "TIME: " + GameManager.Instance.playTime.ToString("f0");
+            gameOverScore = GameObject.Find("txt_TotalScore").GetComponent<Text>();
+            gameOverScore.text = "THE TOTAL SCORE IS " + GameManager.Instance.score + " AND YOU SURVIVED " + GameManager.Instance.playTime.ToString("f2") + " seconds!"; ;
+            gameOverScoreComment = GameObject.Find("txt_TotalScoreComment").GetComponent<Text>();
+            FinalComment();
         }
-
-    }
-
-    private void ChangeTimerColor()
-    {
-        if (livesTextBox != null)
-        {
-            if (GameManager.Instance.lives < 2)
-            {
-                livesTextBox.color = new Color(1, 0, 0);
-            }
-        }
-        
-    }
-
-
- 
-
-    private void FinalComment()
-    {
-        if (gameOverScoreComment == null)
-            return; // Code Blocker
-
-        foreach (var scoreThreshold in scoreComments.Keys)
-        {
-            if (GameManager.Instance.score < scoreThreshold)
-            {
-                gameOverScoreComment.text = scoreComments[scoreThreshold] + " - RESTART SESSION";
-                return;
-            }
-        }
-    }
-
-    public void MinusOneLifeFeedback()
-    {
-        var minusFeedback = Instantiate(minusOneLife, minusOneLifePosition.transform.position, Quaternion.identity) as GameObject;
-        minusFeedback.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
-        minusFeedback.transform.position = minusOneLifePosition.transform.position;
-        Destroy(minusFeedback, 2f);
-
-    }
-
-    public void PlusOneLifeFeedback()
-    {
-        var plusFeedback = Instantiate(plusOneLife, minusOneLifePosition.transform.position, Quaternion.identity) as GameObject;
-        plusFeedback.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
-        plusFeedback.transform.position = minusOneLifePosition.transform.position;
-        Destroy(plusFeedback, 2f);
-    }
-   
-
-    private void Initialisation()
-    {
-        scoreTextBox        = GameObject.Find("txtPro_Score").GetComponent<TextMeshProUGUI>();
-        scoreTextBox.text   = "SCORE:" + GameManager.Instance.score.ToString();
-
-        livesTextBox = GameObject.Find("txtPro_Lives").GetComponent<TextMeshProUGUI>();
-        livesTextBox.text = "LIVES: " + GameManager.Instance.lives.ToString("f0");
-
-        timeTextBox = GameObject.Find("txt_Timer").GetComponent<Text>();      
-
-    }
-
-    private void GameOverInitialisation()
-    {
-        gameOverScore = GameObject.Find("txt_TotalScore").GetComponent<Text>();        
-        gameOverScore.text = "THE TOTAL SCORE IS " + GameManager.Instance.score + " AND YOU SURVIVED " + GameManager.Instance.playTime.ToString("f2") + " seconds!"; ;
-        gameOverScoreComment = GameObject.Find("txt_TotalScoreComment").GetComponent<Text>();             
-        FinalComment();
     }
 }
--- Game Dev 3 - Week 10/Assets/_Scripts/Managers/VariablesSetter.cs
+++ Game Dev 3 - Week 10/Assets/_Scripts/Managers/VariablesSetter.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VariablesSetter : MonoBehaviour
+namespace GameDevWithMarco.Managers
 {
-    [SerializeField] Animator transitionAnim;
-
-    private void Awake()
+    public class VariablesSetter : MonoBehaviour
     {
-        //Sets the fade variable
-        MyScenemanager.Instance.transitionAnim = transitionAnim;
+        [SerializeField] Animator transitionAnim;
+
+        private void Awake()
+        {
+            //Sets the fade variable
+            MyScenemanager.Instance.transitionAnim = transitionAnim;
+        }
     }
 }
